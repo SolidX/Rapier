66cf6b02df8bd43491c3ef4e8191ab5620dd250b
SolidX <gaf256@nyu.edu>
2018-08-27 20:49:50 -0400

    Adds the Minimum Payment repayment strategy.

---
+++ Solidus.Rapier.Core/RepaymentStrategies/MinimumPaymentStrategy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solidus.Rapier.Core
+{
+    /// <summary>
+    /// A Repayment strategy in which you pay no more than the monthly minimum each month.
+    /// It is both the simplest and most ineffective way of doing things.
+    /// </summary>
+    public class MinimumPaymentStrategy : IRepaymentStrategy
+    {
+        public Dictionary<int, Payment> RecommendedPaymentAllocations(IEnumerable<Loan> loans, decimal totalPayment, DateTime paymentDate)
+        {
+            var loansAsOfDt = loans.Select(l => l.ProjectForward(paymentDate));
+            var remainingFunds = totalPayment - loansAsOfDt.Sum(x => x.EffeciveMinimumPayment);
+            var noMinimumLoans = loansAsOfDt.Count(x => x.MinimumPayment <= 0m);
+            return loansAsOfDt.ToDictionary(k => k.Id, v => new Payment { Amount = v.Principal > 0 ? v.EffeciveMinimumPayment : Math.Round(remainingFunds / noMinimumLoans), PaidOn = paymentDate });
+        }
+    }
+}
